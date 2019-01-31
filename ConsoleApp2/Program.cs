103f1bf3a1b7796745442ad6eba1d88467aab540
visualstudio <visualstudio@230-PC15>
2019-01-31 14:05:57 -0500

    2:06

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
+    class village
     {
-        static void Main(string[] args)
+        public static int numberOfVillages = 0;
+        public village nextvillage;
+        public village previousvillage;
+        public string villagename;
+        public bool isAstrildehere = false;
+
+        public village() { village.numberOfVillages++; }
+        class countyside
         {
+            public village Maple;
+            public village Toronto;
+            public village Ajax;
+
+
+            public void MapInitializer()
+            {
+                Maple = new village();
+                Maple.villagename = "Maple";
+                Maple.previousvillage = null;
+                Maple.nextvillage = Toronto;
+                Toronto = new village();
+                Toronto.villagename = "Maple";
+                Toronto.previousvillage = Toronto;
+                Toronto.nextvillage = Ajax;
+                Ajax = new village();
+                Ajax.villagename = "Ajax";
+                Ajax.previousvillage = null;
+                Ajax.nextvillage = Toronto;
+            }
+
         }
+
     }
 }
